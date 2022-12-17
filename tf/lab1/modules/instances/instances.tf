e46b5e8abb9a661e452faf1032588db4ddbc1f6f
pjablonski123 <36215334+pjablonski123@users.noreply.github.com>
2022-12-18 00:23:14 +0100

    Update instances.tf

--- tf/lab1/modules/instances/instances.tf
+++ tf/lab1/modules/instances/instances.tf
@@ -1,6 +1,6 @@
 resource "google_compute_instance" "tf-instance-1" {
   name         = "tf-instance-1"
-  machine_type = "n1-standard"
+  machine_type = "n1-standard-1"
 
   boot_disk {
     initialize_params {
@@ -26,7 +26,7 @@ resource "google_compute_instance" "tf-instance-1" {
 
 resource "google_compute_instance" "tf-instance-2" {
   name         = "tf-instance-2"
-  machine_type = "n1-standard"
+  machine_type = "n1-standard-1"
 
   boot_disk {
     initialize_params {
@@ -48,3 +48,30 @@ resource "google_compute_instance" "tf-instance-2" {
 
   allow_stopping_for_update = true
 }
+/*
+resource "google_compute_instance" "tf-instance-3" {
+  name         = "tf-instance-qwerty"
+  machine_type = "n1-standard-2"
+
+  boot_disk {
+    initialize_params {
+      image  = "https://www.googleapis.com/compute/v1/projects/debian-cloud/global/images/debian-10-buster-v20221206"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata_startup_script = <<-EOT
+      #!/bin/bash
+    EOT
+
+  allow_stopping_for_update = true
+
+}
+*/
