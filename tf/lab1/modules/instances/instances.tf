60a807781a4ce56d3f87a6fbb5e9e4a914c425a8
pjablonski123 <36215334+pjablonski123@users.noreply.github.com>
2022-12-17 22:38:28 +0100

    Update instances.tf

--- tf/lab1/modules/instances/instances.tf
+++ tf/lab1/modules/instances/instances.tf
@@ -23,3 +23,30 @@ resource "google_compute_instance" "vm_instance" {
   allow_stopping_for_update = true
   
 }
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "tf-instance-2"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+  
+  metadata_startup_script = <<-EOT
+      #!/bin/bash
+    EOT
+  
+  allow_stopping_for_update = true
+  
+}
+Footer
