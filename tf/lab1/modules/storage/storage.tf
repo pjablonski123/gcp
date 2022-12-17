447f6e349555842d6cc8be5981b283b5a0638ae1
pjablonski123 <36215334+pjablonski123@users.noreply.github.com>
2022-12-18 00:21:31 +0100

    Update storage.tf

--- tf/lab1/modules/storage/storage.tf
+++ tf/lab1/modules/storage/storage.tf
@@ -1,9 +1,8 @@
 resource "google_storage_bucket" "bucket" {
-  name               = var.name
-  project            = var.project_id
-  location           = var.location
-  storage_class      = var.storage_class
-  labels             = var.labels
-  force_destroy      = var.force_destroy
-  uniform_bucket_level_access = true
+  name = "tf-bucket-830313"
+  location = "US"
+  force_destroy = true
+
+  #uniform_bucket_level_access = true
+
 }
