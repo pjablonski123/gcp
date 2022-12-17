9c32c46590726ea3986f965726a1fd0c0726993f
pjablonski123 <36215334+pjablonski123@users.noreply.github.com>
2022-12-17 21:00:25 +0100

    Update instances.tf

--- tf/lab1/modules/instances/instances.tf
+++ tf/lab1/modules/instances/instances.tf
@@ -1,14 +1,25 @@
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
+  name         = "tf-instance-1"
   machine_type = "f1-micro"
+
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
     }
   }
+
   network_interface {
-    network = google_compute_network.vpc_network.name
+    network = "default"
+
     access_config {
+      // Ephemeral public IP
     }
   }
+  
+  metadata_startup_script = <<-EOT
+        #!/bin/bash
+        EOT
+  
+  allow_stopping_for_update = true
+  
 }
