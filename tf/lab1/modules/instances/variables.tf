ddc6d6388f069680257a2256d92445b4cc5d513d
pjablonski123 <36215334+pjablonski123@users.noreply.github.com>
2022-12-17 20:28:16 +0100

    Create variables.tf

---
+++ tf/lab1/modules/instances/variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  description = "GCP region"
+  type        = string
+}
+variable "zone" {
+  description = "GCP zone"
+  type        = string
+}
+variable "project_id" {
+  description = "GCP project_id"
+  type        = string
+}
