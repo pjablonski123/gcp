064de579eecf1a1600d1d8fc75a51d115b108c6f
pjablonski123 <36215334+pjablonski123@users.noreply.github.com>
2022-12-17 20:32:07 +0100

    Create storage.tf

---
+++ tf/lab1/modules/storage/storage.tf
@@ -0,0 +1,9 @@
+resource "google_storage_bucket" "bucket" {
+  name               = var.name
+  project            = var.project_id
+  location           = var.location
+  storage_class      = var.storage_class
+  labels             = var.labels
+  force_destroy      = var.force_destroy
+  uniform_bucket_level_access = true
+}
