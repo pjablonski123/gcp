b3b8a67bed1a043038c673567899d6c77fcf5f7f
pjablonski123 <36215334+pjablonski123@users.noreply.github.com>
2022-12-17 20:34:34 +0100

    Update variables.tf

--- tf/lab1/modules/storage/variables.tf
+++ tf/lab1/modules/storage/variables.tf
@@ -1,12 +1,15 @@
 variable "region" {
   description = "GCP region"
   type        = string
+  default     = "us-east1"
 }
 variable "zone" {
   description = "GCP zone"
   type        = string
+  default     = "[us-east1-x]"
 }
 variable "project_id" {
   description = "GCP project_id"
   type        = string
+  default     = "[PROJECT_ID]"
 }
