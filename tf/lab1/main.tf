f17f3a98fba1e793b16d77d8ddba983380411816
pjablonski123 <36215334+pjablonski123@users.noreply.github.com>
2022-12-18 00:25:15 +0100

    Update main.tf

--- tf/lab1/main.tf
+++ tf/lab1/main.tf
@@ -5,7 +5,21 @@ terraform {
     }
   }
 }
-
+/*
+terraform {
+  backend "local" {
+    path = "terraform/state/terraform.tfstate"
+  }
+}
+*/
+/*
+terraform {
+  backend "gcs" {
+    bucket  = "tf-bucket-830313"
+    prefix  = "terraform/state"
+  }
+}
+*/
 provider "google" {
   version = "3.5.0"
   project = var.project_id
@@ -16,3 +30,31 @@ provider "google" {
 module "instances" {
   source = "./modules/instances"
 }
+
+module "storage" {
+  source = "./modules/storage"
+}
+
+/*
+resource "google_compute_firewall" "default" {
+  name    = "tf-firewall"
+  network = "tf-vpc-31670"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  direction = "INGRESS"
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_network" "default" {
+  name = "tf-vpc-31670"
+}
+
+module "network" {
+  source  = "terraform-google-modules/network/google"
+  version = "3.4.0"
+  # insert the 3 required variables here
+}*/
